4fb3873b24f124ce278628afc7ba1b7d96ccf298
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-03-15 00:30:38 +0100

    [Chore #164589843] Host app on heroku

---
+++ build/v1/routes/user.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+var _user = _interopRequireDefault(require("../users/user.controller"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var router = _express.default.Router(); // routes
+
+
+router.post('/login', _user.default.authenticate);
+router.post('/signup', _user.default.register);
+router.get('/:id', _user.default.getById);
+router.put('/:id', _user.default.update);
+var _default = router;
+exports.default = _default;
